fff4fa6659fc4b27f9b43ee1b54d80881d01124c
mathehus <matheusqueijo20@gmail.com>
2022-09-27 22:50:33 -0300

    a

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def homepage():
     # return render_template("index.html")
-    return 'A API está no ar' 
+    return 'A API está no ara' 
 
 
 
