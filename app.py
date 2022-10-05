1807b2d1d70708d8dd5e68f006538a34f0d86a5b
mathehus <matheusqueijo20@gmail.com>
2022-10-04 22:56:20 -0300

    Teste Envio de Resposta

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask,request,jsonify
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -17,9 +19,56 @@ def webhooks():
 
 @app.route('/webhooks', methods=['POST'])
 def webhooksPost():
-    print("ENTROU LUISÃO")
-    _json = request.json
-    print(request)
-    print(_json)
-    return "200"
+    _json = request
+    try:
+        menssagem = request['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
+
+        if menssagem == 'Apontar Horas':
+            chamar = "robo"
+            #Validar campos
+            return "200"
+        else:
+             
+            url = "https://graph.facebook.com/v14.0/101101909440708/messages"
+            
+            headers = {"Content-Type": "application/json; charset=utf-8", "Authorization" : "Bearer EAALVmdug7uMBAPfV18nvkifWp3vpWQ8LfQ6rjdBFrOd0xTfDjaZArIrdzi97emmuX30q8ANr1SdCZAoKlqTLGRqNkZCKiNel7ZCofnfm1fR6TljzX7LmjFLpB6jgPMa4ZCC2HjWKr8nv4kES97y6HEAc4Atz4imUE5mV28nZCEJQ1IuxM2dnXR"}
+            
+            data = {
+                "messaging_product": "whatsapp",
+                "to": "5511962583347",
+                "type": "template",
+                "template": {
+                    "name": "timesheet",
+                    "language": {
+                    "code": "pt_BR"
+                    }
+                }
+            }
+            
+            response = requests.post(url, headers=headers, json=data)
+            return "500"
+    except:
+        url = "https://graph.facebook.com/v14.0/101101909440708/messages"
+            
+        headers = {"Content-Type": "application/json; charset=utf-8", "Authorization" : "Bearer EAALVmdug7uMBAPfV18nvkifWp3vpWQ8LfQ6rjdBFrOd0xTfDjaZArIrdzi97emmuX30q8ANr1SdCZAoKlqTLGRqNkZCKiNel7ZCofnfm1fR6TljzX7LmjFLpB6jgPMa4ZCC2HjWKr8nv4kES97y6HEAc4Atz4imUE5mV28nZCEJQ1IuxM2dnXR"}
+            
+        data = {
+            "messaging_product": "whatsapp",
+             "to": "5511962583347",
+             "type": "template",
+             "template": {
+                "name": "timesheet",
+                 "language": {
+                "code": "pt_BR"
+                }
+            }
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+        return "500"
+         #mandar erro pro wpp  
+
+
+   
+   
 
