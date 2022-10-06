6fba205d60c834b7774a6659e8b394fe52b684aa
Matheus Santos <65913769+mathehus@users.noreply.github.com>
2022-10-06 10:24:23 -0300

    agora vai

--- app.py
+++ app.py
@@ -21,35 +21,8 @@ def webhooks():
 def webhooksPost():
     _json = request.json
 
-    menssagem = _json['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
-
-    if menssagem == 'Apontar Horas':
-            chamar = "robo"
-            print("robo")
-            #Validar campos
-            return "200"
-    else:
-             
-            url = "https://graph.facebook.com/v14.0/101101909440708/messages"
-            
-            headers = {"Content-Type": "application/json; charset=utf-8", "Authorization" : "Bearer EAALVmdug7uMBAPfV18nvkifWp3vpWQ8LfQ6rjdBFrOd0xTfDjaZArIrdzi97emmuX30q8ANr1SdCZAoKlqTLGRqNkZCKiNel7ZCofnfm1fR6TljzX7LmjFLpB6jgPMa4ZCC2HjWKr8nv4kES97y6HEAc4Atz4imUE5mV28nZCEJQ1IuxM2dnXR"}
-            
-            data = {
-                "messaging_product": "whatsapp",
-                "to": "5511962583347",
-                "type": "template",
-                "template": {
-                    "name": "timesheet",
-                    "language": {
-                    "code": "pt_BR"
-                    }
-                }
-            }
-            
-           # response = requests.post(url, headers=headers, json=data)
-            print("500")
-            return "500"
-    
+    print(_json)
+    return "200"
 
 
 
