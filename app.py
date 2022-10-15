e17d1c300ca42d7c992ce04cf7dffdd219bfc514
Matheus Santos <65913769+mathehus@users.noreply.github.com>
2022-10-15 16:03:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def robo(arrayMenssagem):
         op.add_argument("--no-sandbox")
         op.add_argument("--disable-dev-sh-usage")
         
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options= op)
+        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=op)
         print("ABRIU")
 
         #logando
